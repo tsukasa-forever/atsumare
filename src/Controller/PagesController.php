1ada20f6a00002f56249efe5cb6c102c5af5fb72
tsukasa-forever <engineer.tsukasa@gmail.com>
2020-06-30 20:32:57 +0900

    fix component

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -45,29 +45,6 @@ class PagesController extends AppController
      */
     public function display(...$path): ?Response
     {
-        if (!$path) {
-            return $this->redirect('/');
-        }
-        if (in_array('..', $path, true) || in_array('.', $path, true)) {
-            throw new ForbiddenException();
-        }
-        $page = $subpage = null;
-
-        if (!empty($path[0])) {
-            $page = $path[0];
-        }
-        if (!empty($path[1])) {
-            $subpage = $path[1];
-        }
-        $this->set(compact('page', 'subpage'));
-
-        try {
-            return $this->render(implode('/', $path));
-        } catch (MissingTemplateException $exception) {
-            if (Configure::read('debug')) {
-                throw $exception;
-            }
-            throw new NotFoundException();
-        }
+        
     }
 }
