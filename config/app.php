3deb23ca9c20c9f1f0f74758e61604c92bc14811
tsukasa-forever <engineer.tsukasa@gmail.com>
2020-06-30 20:23:13 +0900

    add profile

--- config/app.php
+++ config/app.php
@@ -341,30 +341,42 @@ return [
     /*
      * Configures logging options
      */
+//    'Log' => [
+//        'debug' => [
+//            'className' => FileLog::class,
+//            'path' => LOGS,
+//            'file' => 'debug',
+//            'url' => env('LOG_DEBUG_URL', null),
+//            'scopes' => false,
+//            'levels' => ['notice', 'info', 'debug'],
+//        ],
+//        'error' => [
+//            'className' => FileLog::class,
+//            'path' => LOGS,
+//            'file' => 'error',
+//            'url' => env('LOG_ERROR_URL', null),
+//            'scopes' => false,
+//            'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
+//        ],
+//        // To enable this dedicated query log, you need set your datasource's log flag to true
+//        'queries' => [
+//            'className' => FileLog::class,
+//            'path' => LOGS,
+//            'file' => 'queries',
+//            'url' => env('LOG_QUERIES_URL', null),
+//            'scopes' => ['queriesLog'],
+//        ],
+//    ],
     'Log' => [
         'debug' => [
-            'className' => FileLog::class,
-            'path' => LOGS,
-            'file' => 'debug',
-            'url' => env('LOG_DEBUG_URL', null),
-            'scopes' => false,
+            'className' => 'Cake\Log\Engine\ConsoleLog',
+            'stream' => 'php://stdout',
             'levels' => ['notice', 'info', 'debug'],
         ],
         'error' => [
-            'className' => FileLog::class,
-            'path' => LOGS,
-            'file' => 'error',
-            'url' => env('LOG_ERROR_URL', null),
-            'scopes' => false,
+            'className' => 'Cake\Log\Engine\ConsoleLog',
+            'stream' => 'php://stderr',
             'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
-        ],
-        // To enable this dedicated query log, you need set your datasource's log flag to true
-        'queries' => [
-            'className' => FileLog::class,
-            'path' => LOGS,
-            'file' => 'queries',
-            'url' => env('LOG_QUERIES_URL', null),
-            'scopes' => ['queriesLog'],
         ],
     ],
 
